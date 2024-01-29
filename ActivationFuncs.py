c09e1980440e5fef4c79ab8191a2b52b863dd166
DanielErb <erbesfel@post.bgu.ac.il>
2024-01-29 20:47:03 +0200

    added neural network

--- ActivationFuncs.py
+++ ActivationFuncs.py
@@ -8,31 +8,31 @@ class Softmax:
         self.W = W
         self.b = b
 
-    def activation(self, X, W = None, b=None):
-        if(W is None):
+    def activation(self, X, W=None, b=None):
+        if (W is None):
             W = self.W
-        if(b is None):
+        if (b is None):
             b = self.b
         temp = np.dot(X.T, W) + b
-        #sum = np.sum(np.exp(temp), axis=1)
+        # sum = np.sum(np.exp(temp), axis=1)
         return np.exp(temp) / np.sum(np.exp(temp), axis=1, keepdims=True)
 
-    def loss(self,X, Y, W=None, b=None):
-        if(W is None):
+    def loss(self, X, Y, W=None, b=None):
+        if (W is None):
             W = self.W
-        if(b is None):
+        if (b is None):
             b = self.b
         m = X.shape[1]
         # print("y*log", y * np.log(softmax(W, X)))
         # print("-1/m", -1/m*(y * np.log(softmax(W, X))))
         loss_check = -1 / m * (Y * np.log(self.activation(X, W, b))).sum()
-        #print("loss_check", loss_check)
+        # print("loss_check", loss_check)
         return loss_check
 
-    def gradient(self, X, Y, W = None, b = None):
-        if(W is None):
+    def gradient(self, X, Y, W=None, b=None):
+        if (W is None):
             W = self.W
-        if(b is None):
+        if (b is None):
             b = self.b
         # temp = np.dot(X.T, W)
         # after_softmax =  np.exp(temp). / sum(np.exp(temp), axis=0)
@@ -44,80 +44,62 @@ class Softmax:
         dx = np.dot(W, (after_softmax - Y).T) / X.shape[1]
         # print("dx.shape", dx.shape)
         db = np.sum((after_softmax - Y), axis=0, keepdims=True) / X.shape[1]
-        #print("db", db)
+        # print("db", db)
         # print("db.shape", db.shape)
         # grad = np.concatenate((dw, db), axis=0)
         return dw, dx, db
 
 
-class tanh:
-    def __init__(self, W, b):
+class LayerFunc:
+    def __init__(self, W, b, Activation):
         # need to save only W and b because these are the only parameters that we are going to change
         # and X and Y are just sampled per each approch so we dont need to save them
-        self.W = W
-        self.b = b
-
-    def activation(self, X, W = None, b=None):
-        if(W is None):
-            W = self.W
-        if(b is None):
-            b = self.b
-        return np.tanh(np.dot(W, X) + b)
-    
-    def gradient(self, X, V, W = None, b = None):
-        if(W is None):
-            W = self.W
-        if(b is None):
-            b = self.b
-        dw = 
-
-class ActiovationFunc:
-    def __init__(self, W, b, Activation , Derrative):
-        # need to save only W and b because these are the only parameters that we are going to change
-        # and X and Y are just sampled per each approch so we dont need to save them
-        if self.Activate.lower() != "tanh" && self.Activate.lower() != "relu":
+        if Activation.lower() != "tanh" and Activation.lower() != "relu":
             raise ValueError(f"Invalid input: '{Activation}'. Please provide Relu or Tanh")
         self.W = W
         self.b = b
         self.Activate = Activation
-        self.Derrative = Derrative
-    
-    def activation(self, X, W = None, b=None):
-        if(W is None):
+
+    def activation(self, X, W=None, b=None):
+        if (W is None):
             W = self.W
-        if(b is None):
+        if (b is None):
             b = self.b
-        
+
         if self.Activate.lower() == "tanh":
             return np.tanh(np.dot(W, X) + b)
-        
-        return reluActivation(np.dot(W, X) + b)
-         
-    def reluActivation(self, X, W = None, b=None):
-        if(W is None):
+
+        return self.reluActivation(X)
+
+    def reluActivation(self, X, W=None, b=None):
+        if (W is None):
             W = self.W
-        if(b is None):
+        if (b is None):
             b = self.b
         return np.maximum(0, np.dot(W, X) + b)
-    
-    
-    def reluDerrative(t):
+
+    def reluDerivative(self, t):
         return np.where(t > 0, 1, 0)
 
-    
-    def tanhDerrative(t):
-       
-        return 1 - np.tanh(t)**2
-    
-    def gradient(self, X, V, W = None, b = None):
-        if(W is None):
-            W = self.W
-        if(b is None):
-            b = self.b
-        derrative = if self.Activate.lower() == "tanh" tanhDerrative else reluDerrative
-        dbV = derrative(np.dot(W, X) + b) * V
-        dwV = np.dot((derrative(np.dot(W, X) + b) * V) , X.T)
-        dxV =  np.dot(W.T , (derrative(np.dot(W, X) + b) * V))
-        return dwV, dxV, dbV
-    
+    def tanhDerivative(self, t):
 
+        return 1 - np.tanh(t) ** 2
+
+    def gradient(self, X, V, W=None, b=None):
+        if (W is None):
+            W = self.W
+        if (b is None):
+            b = self.b
+
+        derivative = None
+        if self.Activate == "relu":
+            derivative = self.reluDerivative
+        else:
+            derivative = self.tanhDerivative
+
+        output_before_activation = np.dot(W, X) + b
+        dbV = np.sum(derivative(output_before_activation) * V, axis=1, keepdims=True)
+        print("dbV", dbV)
+        dwV = np.dot((derivative(output_before_activation) * V), X.T)
+        dxV = np.dot(W.T, (derivative(output_before_activation) * V))
+        return dwV, dxV, dbV
--- jacobianTest.py
+++ jacobianTest.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ActivationFuncs import ActiovationFunc
+from ActivationFuncs import LayerFunc
 
 
 def normalize(v):
@@ -23,11 +23,86 @@ def jacobian_CheckX(W, X, b, check):
     u = np.random.randn(W.shape[0], X.shape[1])
     for i in range(1, 20):
         epsilon = (0.5 ** i)
-        dw, dx, db = check.gradient(X, u)
+        dwu, dxu, dbu = check.gradient(X, u)
         activation_epsilon = np.vdot(u, check.activation(X + epsilon * d, W, b))
         activation_regular = np.vdot(u, check.activation(X))
         difference = np.abs(activation_epsilon - activation_regular)
-        difference_with_grad = np.abs(activation_epsilon - activation_regular - np.vdot(dx, epsilon * d))
+        difference_with_grad = np.abs(activation_epsilon - activation_regular - np.vdot(dxu, epsilon * d))
+        epsilons.append(epsilon)
+        differences.append(difference)
+        differences_with_grad.append(difference_with_grad)
+        print("epsilon", epsilon)
+        print("difference", difference)
+        print("difference_with_grad", difference_with_grad)
+
+    print("difference", difference)
+    print("difference_with_grad", difference_with_grad)
+    plt.loglog(epsilons, differences, label='Without Gradient')
+    plt.loglog(epsilons, differences_with_grad, label='With Gradient')
+
+    plt.xlabel('Epsilon')
+    plt.ylabel('Difference')
+
+    plt.legend()
+    plt.title('Difference by Epsilon')
+    plt.gca().invert_xaxis()
+    plt.show()
+
+
+def jacobian_CheckW(W, X, b, check):
+    d = normalize(np.random.randn(W.shape[0], W.shape[1]))
+    # print("d", d)
+    # print(W.shape)
+    epsilon = 1
+    epsilons = []
+    differences = []
+    differences_with_grad = []
+    u = np.random.randn(W.shape[0], X.shape[1])
+    for i in range(1, 20):
+        epsilon = (0.5 ** i)
+        dwu, dxu, dbu = check.gradient(X, u)
+        activation_epsilon = np.vdot(u, check.activation(X, W + epsilon * d, b))
+        activation_regular = np.vdot(u, check.activation(X))
+        difference = np.abs(activation_epsilon - activation_regular)
+        difference_with_grad = np.abs(activation_epsilon - activation_regular - np.vdot(dwu, epsilon * d))
+        epsilons.append(epsilon)
+        differences.append(difference)
+        differences_with_grad.append(difference_with_grad)
+        print("epsilon", epsilon)
+        print("difference", difference)
+        print("difference_with_grad", difference_with_grad)
+
+    print("difference", difference)
+    print("difference_with_grad", difference_with_grad)
+    plt.loglog(epsilons, differences, label='Without Gradient')
+    plt.loglog(epsilons, differences_with_grad, label='With Gradient')
+
+    plt.xlabel('Epsilon')
+    plt.ylabel('Difference')
+
+    plt.legend()
+    plt.title('Difference by Epsilon')
+    plt.gca().invert_xaxis()
+    plt.show()
+
+
+def jacobian_Checkb(W, X, b, check):
+    d = normalize(np.random.randn(b.shape[0], b.shape[1]))
+    # print("d", d)
+    # print(W.shape)
+    epsilon = 1
+    epsilons = []
+    differences = []
+    differences_with_grad = []
+    u = np.random.randn(W.shape[0], X.shape[1])
+    for i in range(1, 20):
+        epsilon = (0.5 ** i)
+        dwu, dxu, dbu = check.gradient(X, u)
+        activation_epsilon = np.vdot(u, check.activation(X, W, b + epsilon * d))
+        activation_regular = np.vdot(u, check.activation(X))
+        difference = np.abs(activation_epsilon - activation_regular)
+        print("dbu.shape", dbu.shape)
+        difference_with_grad = np.abs(activation_epsilon - activation_regular - np.vdot(dbu, epsilon * d))
         epsilons.append(epsilon)
         differences.append(difference)
         differences_with_grad.append(difference_with_grad)
@@ -52,14 +127,14 @@ def jacobian_CheckX(W, X, b, check):
 def main():
     W = np.random.randn(5, 5)  # Example weights
     X = np.random.randn(5, 3)  # Example input
-    b = np.random.randn(1, 5)  # Example biases
+    b = np.random.randn(5, 1)  # Example biases
     # y is supposed to be 3 5
     y = np.array([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0]])  # Example one-hot encoded labels
 
-    check = Softmax(W, b)
+    check = LayerFunc(W, b, "tanh")
 
-    jacobian_CheckX(W, X, b, check)
+    jacobian_Checkb(W, X, b, check)
 
 
 if __name__ == '__main__':
-    main(
+    main()
