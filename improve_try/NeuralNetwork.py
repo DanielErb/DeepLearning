25af410ef89d52232d78f8e2acb20c6476e26329
DanielErb <erbesfel@post.bgu.ac.il>
2024-01-30 22:33:15 +0200

    finished ResNet add in NeuralNetwork.py

--- improve_try/NeuralNetwork.py
+++ improve_try/NeuralNetwork.py
@@ -10,11 +10,13 @@ import numpy as np
 
 
 class NeuralNetwork:
-    def __init__(self, numLayers = defalut_conf.numLayers, activationFuncName = defalut_conf.activationFuncName,
-                 learning_rate = defalut_conf.learning_rate, epochs = defalut_conf.num_epochs,
-                 batch_size = defalut_conf.batch_size, Y_train = defalut_conf.Y_train, X_train = defalut_conf.X_train,
-                 Y_test = defalut_conf.Y_test, X_test = defalut_conf.X_test,
-               accuracy_sample_size_train = defalut_conf.accuracy_sample_size_train, accuracy_sample_size_test = defalut_conf.accuracy_sample_size_test , networkType = defalut_conf.NetworkType):
+    def __init__(self, numLayers=defalut_conf.numLayers, activationFuncName=defalut_conf.activationFuncName,
+                 learning_rate=defalut_conf.learning_rate, epochs=defalut_conf.num_epochs,
+                 batch_size=defalut_conf.batch_size, Y_train=defalut_conf.Y_train, X_train=defalut_conf.X_train,
+                 Y_test=defalut_conf.Y_test, X_test=defalut_conf.X_test,
+                 accuracy_sample_size_train=defalut_conf.accuracy_sample_size_train,
+                 accuracy_sample_size_test=defalut_conf.accuracy_sample_size_test,
+                 networkType=defalut_conf.NetworkType):
         self.numLayers = numLayers
         self.activationFuncName = activationFuncName
         self.learning_rate = learning_rate
@@ -41,26 +43,39 @@ class NeuralNetwork:
                 if self.numLayers == 1:
                     w = np.random.randn(self.X_train.shape[0], self.Y_train.shape[1])
                 else:
-                    w = np.random.randn(self.weights[i - 2].shape[0], self.Y_train.shape[1])
+                    if self.networkType == "standard":
+                        w = np.random.randn(self.weights[i - 2].shape[0], self.Y_train.shape[1])
+                    else:
+                        w = np.random.randn(self.weights[i - 2].shape[1], self.Y_train.shape[1])
                 b = np.random.randn(1, self.Y_train.shape[1])
                 self.weights.append(w)
                 self.biases.append(b)
-                self.layers.append(Softmax(W = w, b = b , networkType = self.networkType))
+                self.layers.append(Softmax(W=w, b=b))
 
             elif i == 1:  # at least 2 layers
                 num_weights = random.randint(50, 100)
                 w = np.random.randn(num_weights, self.X_train.shape[0])
                 b = np.random.randn(num_weights, 1)
+                if self.networkType == "ResNet":
+                    w2 = np.random.randn(self.X_train.shape[0], num_weights)
+                    self.weights2.append(w2)
                 self.weights.append(w)
                 self.biases.append(b)
-                self.layers.append(LayerFunc(W = w, b = b, Activation = self.activationFuncName , networkType = self.networkType))
+                self.layers.append(
+                    LayerFunc(W=w, b=b, Activation=self.activationFuncName, networkType=self.networkType, W2=w2))
             else:
                 num_weights = random.randint(50, 100)
-                w = np.random.randn(num_weights, self.weights[i - 2].shape[0]) #-2 because i starts from 1
                 b = np.random.randn(num_weights, 1)
+                if self.networkType == "standard":
+                    w = np.random.randn(num_weights, self.weights[i - 2].shape[0])  # -2 because i starts from 1
+                if self.networkType == "ResNet":
+                    w = np.random.randn(num_weights, self.weights[i - 2].shape[1])
+                    w2 = np.random.randn(self.weights[i - 2].shape[1], num_weights)
+                    self.weights2.append(w2)
                 self.weights.append(w)
                 self.biases.append(b)
-                self.layers.append(LayerFunc(W = w, b = b, Activation = self.activationFuncName , networkType = self.networkType))
+                self.layers.append(
+                    LayerFunc(W=w, b=b, Activation=self.activationFuncName, networkType=self.networkType, W2=w2))
 
         # for i in range(1, self.numLayers + 1):
         #     print("layer", i)
@@ -77,8 +92,8 @@ class NeuralNetwork:
 
         plt.plot(range(1, self.epochs + 1), train_accuracy, label='Train Accuracy')
         plt.plot(range(1, self.epochs + 1), test_accuracy, label='Test Accuracy')
-        #plt.plot(range(1, self.epochs + 1), train_loss, label='Train Loss')
-        #plt.plot(range(1, self.epochs + 1), test_loss, label='Test Loss')
+        # plt.plot(range(1, self.epochs + 1), train_loss, label='Train Loss')
+        # plt.plot(range(1, self.epochs + 1), test_loss, label='Test Loss')
         plt.xlabel('Epoch')
         plt.ylabel('Percent')
         plt.title('Success Percent in the train set per epoch')
